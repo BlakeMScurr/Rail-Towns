0152e02ad702f05fa78eebc0486a8f87cb12a5fe
Blake McAlevey-Scurr <blakemscurr@gmail.com>
2024-10-31 10:44:00 +1300

    Fix deploy issue.

--- src/components/DevelopmentCalculator.tsx
+++ src/components/DevelopmentCalculator.tsx
@@ -27,7 +27,7 @@ export default function DevelopmentCalculator({}: Props) {
         <canvas id="overviewCanvas"></canvas>
         <div id="detailCanvas"></div>
 
-        <script type="module" src="https://cdnjs.cloudflare.com/ajax/libs/three.js/0.169.0/three.module.min.js" integrity="sha512-fFc6JwwOG7dmtOGpA/X3+HGW1XdDp7818iuqwDEx14Imh4QSh7q91BPz4EdNeHR8lLDaMRYuSWqv7Ts2dbZa1Q==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+        <script type="module" src="https://cdnjs.cloudflare.com/ajax/libs/three.js/0.169.0/three.module.min.js" integrity="sha512-fFc6JwwOG7dmtOGpA/X3+HGW1XdDp7818iuqwDEx14Imh4QSh7q91BPz4EdNeHR8lLDaMRYuSWqv7Ts2dbZa1Q==" crossOrigin="anonymous" referrerpolicy="no-referrer"></script>
         <script type="module" src="/js/devcalc.js"></script>
     </div>
 }
